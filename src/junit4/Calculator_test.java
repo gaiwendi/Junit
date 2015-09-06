397dacdb619e59a03cbbd1f6f6189b6d0b9b47ae
程序猿-硕 <-@172.22.37.35>
2015-09-06 13:42:36 +0800

    单元测试3

---
+++ src/junit4/Calculator_test.java
@@ -0,0 +1,14 @@
+package junit4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calculator_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
